ec5152fe1b92bd22002cfb9a87f755243ce43ec4
Rego <pvrego@users.noreply.github.com>
2019-02-28 07:40:46 -0300

    Update server-pthread.cpp

--- source/server-pthread.cpp
+++ source/server-pthread.cpp
@@ -68,20 +68,56 @@ int main(int argc , char *argv[]){
 /*
  * This will handle connection for each client
  * */
+//void *connection_handler(void *socket_desc){
+	//Get the socket descriptor
+	//int sock = *(int*)socket_desc;
+	
+	//char *message;
+	
+	//Send some messages to the client
+	//message = "Greetings! I am your connection handler\n";
+	//write(sock , message , strlen(message));
+	
+	//message = "Its my duty to communicate with you";
+	//write(sock , message , strlen(message));
+	
+	//Free the socket pointer
+	//free(socket_desc);
+	
+	//return 0;
+//}
+
+/*
+ * This will handle connection for each client
+ * */
 void *connection_handler(void *socket_desc)
 {
 	//Get the socket descriptor
 	int sock = *(int*)socket_desc;
-	
-	char *message;
+	int read_size;
+	char *message , client_message[2000];
 	
 	//Send some messages to the client
 	message = "Greetings! I am your connection handler\n";
 	write(sock , message , strlen(message));
 	
-	message = "Its my duty to communicate with you";
+	message = "Now type something and i shall repeat what you type \n";
 	write(sock , message , strlen(message));
 	
+	//Receive a message from client
+	while ((read_size = recv(sock , client_message , 2000 , 0)) > 0){
+		//Send the message back to client
+		write(sock , client_message , strlen(client_message));
+	}
+	
+	if(read_size == 0){
+		puts("Client disconnected");
+		fflush(stdout);
+	}
+	else if(read_size == -1){
+		perror("recv failed");
+	}
+		
 	//Free the socket pointer
 	free(socket_desc);
 	
